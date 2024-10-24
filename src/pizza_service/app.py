48deeb8fa6bb929a6fd8bfd82260775b671ae4e8
Dimcheck <dfarinyak@gmail.com>
2024-10-24 17:05:42 +0300

    Add main endpoints

--- src/pizza_service/app.py
+++ src/pizza_service/app.py
@@ -1,14 +1,29 @@
+import json
 from fastapi import FastAPI
+import uvicorn
+from src.pizza_service import service
+from threading import Thread
 
-app = FastAPI()
-pizzas = []
-
-
-@app.get("/")
-def read_root():
-    return "yo"
+app = FastAPI(__name__)
 
 
-@app.post("/order/{pizza_id}")
-def generate_first_layer(pizza_id: int):
-    ...
+@app.post("/order/{count}")
+def order_pizzas(count):
+    """order specific amount of pizzas with random fillings"""
+    order_id = service.order_pizzas(int(count))
+    return json.dumps({"order_id": order_id})
+
+
+@app.get("/order/{order_id}")
+def get_order(order_id):
+    return service.get_order(order_id)
+
+
+@app.on_event("startup")
+def launch_consumer():
+    second_thread = Thread(target=service.load_orders)
+    second_thread.start()
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
