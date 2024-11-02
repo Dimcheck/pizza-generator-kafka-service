2599ce46d3cd051406a34fdaa27223cf1523ca77
Dimcheck <30938569+Dimcheck@users.noreply.github.com>
2024-11-02 02:25:49 +0200

    htmx + movie_service :art: (#3)
    * Add movie_service; Refactoring
    
    * Remove deprecated service
    
    * Improve Communication class; Refactoring

--- src/pizza_service/apis/utils.py
+++ src/pizza_service/apis/utils.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from configparser import ConfigParser
 
 ENV_PATH = str(list(Path(__file__).parents)[1] / "configs" / ".env")
 
@@ -13,4 +14,17 @@ def read_env():
                 os.environ[key] = value
 
 
+def make_config(pathfile: str) -> dict:
+    config_parser = ConfigParser()
+    config_parser.read(pathfile)
+    producer_config = dict(config_parser["kafka_client"])
+    consumer_config1 = dict(config_parser["kafka_client"])
+    consumer_config2 = dict(config_parser["kafka_client"])
 
+    consumer_config1.update(config_parser["consumer1"])
+    consumer_config2.update(config_parser["consumer2"])
+    return {
+        "producer": producer_config,
+        "consumer1": consumer_config1,
+        "consumer2": consumer_config2,
+    }
