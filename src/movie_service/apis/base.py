56b377f60702dc5fe62ee7c2f614e18268ffedd2
Dimcheck <dfarinyak@gmail.com>
2024-11-24 18:59:03 +0200

    Remove unnecessary deps :heavy_minus_sign:

--- src/movie_service/apis/base.py
+++ src/movie_service/apis/base.py
@@ -4,8 +4,6 @@ import urllib.error as u_error
 import urllib.parse as u_parse
 import urllib.request as u_request
 
-from fastapi.exceptions import HTTPException
-
 
 class Communication:
     def __init__(self, url):
@@ -17,6 +15,7 @@ class Communication:
             try:
                 with u_request.urlopen(self.url, timeout=timeout) as response:
                     data = response.read()
+                    print(data.decode())
                     return json.loads(data.decode())
             except u_error.HTTPError as e:
                 if e.code == 524:
@@ -24,10 +23,10 @@ class Communication:
                     time.sleep(5)
                     print("Caught 524 error..")
                 else:
-                    raise HTTPException(e.code, e.reason)
+                    raise
 
-            except u_error.URLError as e:
-                raise HTTPException(e.code, e.reason)
+            except u_error.URLError:
+                raise
 
     def get_response_with_params(self, retries: int = 3, **kwargs):
         self.url = f"{self.url}?{u_parse.urlencode(kwargs)}"
